f10ca029b52785b0102cc424b76f5119c648c455
Pedro Augusto <pedroameloborges@gmail.com>
2024-08-29 18:08:13 -0300

    Inserindo Update e Read

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -19,36 +19,41 @@ public class UserTest {
 
 
         int opcao = 0;
-        while(opcao != 6){
+        while (opcao != 6) {
             showMenu();
             opcao = input.nextInt();
 
 
-            switch (opcao){
-                case 1: addUser(users, input);
+            switch (opcao) {
+                case 1:
+                    addToList(users, createUser(input));
                     break;
 
                 case 2:
-                    removeUser(users, input);
+                    readUsers(users);
+
+                case 3:
+                    updateUser(users, input);
                     break;
 
+                case 4:
+                    deleteUser(users, input);
+                    break;
             }
 
         }
-        }
-
-
-    private static void showMenu (){
-        System.out.println("1 - Inserir usuario: ");
-        System.out.println("2- Remover usuario:");
-        System.out.println("3- Deletar usuario");
-        System.out.println("4- Atualizar usuario: ");
     }
 
-    private static void addUser (List<User> list, Scanner input){
+    private static void showMenu() {
+        System.out.println("1 - Criar usuario: ");
+        System.out.println("2- Imprimir usuario:");
+        System.out.println("4- Atualizar usuario: ");
+        System.out.println("3- Deletar usuario");
+    }
+
+    private static User createUser(Scanner input) {
 
         System.out.println("Insira as seguintes informaçoes: ");
-
 
         System.out.println("Name: ");
         input.nextLine();
@@ -60,7 +65,7 @@ public class UserTest {
 
         System.out.println("Data de nascimento (dd/MM/yyyy): ");
         String dateInput = input.nextLine();
-        LocalDate birthday = LocalDate.parse( dateInput, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalDate birthday = LocalDate.parse(dateInput, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
         System.out.println("Password: ");
         String password = input.nextLine();
@@ -69,23 +74,30 @@ public class UserTest {
         System.out.println("Confirm password ");
         String passwordConfirm = input.nextLine();
 
-
-        Predicate<String> passwordMatches = confirm -> passwordConfirm.equals(password);
-
-       if (!passwordMatches.test(passwordConfirm)){
-           throw new IllegalArgumentException("Senhas não coincidem");
-       }
-
+        if (!password.equalsIgnoreCase(passwordConfirm)) {
+            throw new IllegalArgumentException("Senhas não coincidem");
+        }
 
         User user = new User(name, email, password, birthday);
         System.out.println(user);
-        list.add(user);
+
+        return user;
 
     }
 
-    private static void removeUser (List<User> users, Scanner input){
+    private static void readUsers(List<User> users) {
 
-        if(users.isEmpty()){
+        if (users.isEmpty()) {
+            throw new NoSuchElementException("Lista vazia");
+        } else {
+            users.forEach(System.out::println);
+        }
+
+    }
+
+    private static void deleteUser(List<User> users, Scanner input) {
+
+        if (users.isEmpty()) {
             throw new NoSuchElementException("List is empty");
         }
 
@@ -95,14 +107,55 @@ public class UserTest {
 
         boolean exists = users.stream().
                 anyMatch(byName(nome));
-        if (!exists){
+        if (!exists) {
             throw new NoSuchElementException("User inexistente");
         }
 
         users.removeIf(byName(nome));
     }
 
-     private static Predicate<User> byName (String name){
+    private static void updateUser(List<User> users, Scanner input) {
+
+        if (users.isEmpty()) {
+            throw new NoSuchElementException("List is empty");
+        }
+
+        System.out.println("Insira o nome do user a ser atualizado: ");
+
+        input.nextLine();
+        String nome = input.nextLine();
+
+        Optional<User> userFound = users
+                .stream()
+                .filter(byName(nome))
+                .findFirst();
+
+
+        if (userFound.isEmpty()) {
+            throw new NoSuchElementException("Usuario ou nome não correspondente");
+        }
+
+
+        System.out.println("Usuario existente!");
+
+
+        //pegando o usuario com base no index para o set
+        int indexOf = users.indexOf(userFound.get());
+
+        User user = createUser(input);
+
+        //seta o usuario
+        users.set(indexOf, user);
+        System.out.println("Usuario inserido com sucesso!");
+        System.out.println(users);
+
+    }
+
+    private static void addToList(List<User> users, User user) {
+        users.add(user);
+    }
+
+    private static Predicate<User> byName(String name) {
         return user -> user.getName().equals(name);
-     }
+    }
 }
